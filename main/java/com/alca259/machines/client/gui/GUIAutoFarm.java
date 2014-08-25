7303dd4244785fbb096f4d5c933ad76ce26f1fc7
Alcalini <alca259@gmail.com>
2014-08-25 21:00:20 +0200

    Gui text

--- main/java/com/alca259/machines/client/gui/GUIAutoFarm.java
+++ main/java/com/alca259/machines/client/gui/GUIAutoFarm.java
@@ -7,6 +7,7 @@ import com.alca259.machines.tileentity.TileEntityAutoFarmer;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.StatCollector;
 
 public class GUIAutoFarm extends GuiContainer {
 
@@ -22,6 +23,15 @@ public class GUIAutoFarm extends GuiContainer {
 	}
 
 	@Override
+    protected void drawGuiContainerForegroundLayer(int param1, int param2) {
+        //draw text and stuff here
+        //the parameters for drawString are: string, x, y, color
+		this.fontRendererObj.drawString(StatCollector.translateToLocal("tile.alca259AutoFarmer.name"), 8, 6, 4210752);
+        //draws "Inventory" or your regional equivalent
+        this.fontRendererObj.drawString(StatCollector.translateToLocal("container.inventory"), 8, ySize - 96 + 2, 4210752);
+    }
+	
+	@Override
 	protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
 		/*
 		 * Draw GUI Background
@@ -34,6 +44,6 @@ public class GUIAutoFarm extends GuiContainer {
 		// set the y for the texture, Total height - textureSize - 30 (up) / 2,
 		int j = (this.height - ySize) / 2;
 		// draw the texture
-		drawTexturedModalRect(par2, j, 0, 0, xSize, ySize);
+		this.drawTexturedModalRect(par2, j, 0, 0, xSize, ySize);
 	}
 }
